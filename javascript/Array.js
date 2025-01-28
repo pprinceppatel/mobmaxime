90b94af999b3f411caf712547e26bf50e0561dd7
pprinceppatel <pprinceppatel8383@gmail.com>
2025-01-28 18:59:14 +0530

    commit code

--- javascript/Array.js
+++ javascript/Array.js
@@ -2,7 +2,63 @@
 
 => Array : it is same as object. collection of data.
 
+=> array mathods :
+        
+Array length  :  for find array length         
+Array toString() : for convert to string(without couma"" )
+Array at()  : to find exect array
+Array join() : join somthing between arrays
+Array pop() : change,update,delete data.   using principal of lilo
+Array push() : change,update,delete data.  "
+Array shift() :  "         "               using pricipal of fifo
+Array unshift() :  "       "                "       "
+Array delete() :  delete data 
+Array concat() : add two array name.concat(name)
+Array copyWithin() : 
+Array flat()
+Array splice()
+Array toSpliced()
+Array slice()
 
+
+
+
+
+=> immutably change specific data with using slice(). 
+
+const users = [
+    {
+        name : 'prince',
+        age: 22,
+        gender:'male'
+    },
+    {
+        name : 'sam',
+        age : 24,
+        gender : 'female'
+    },
+    {
+        name : "pritvi",
+        age : 20,
+        gender:'unkonwn'
+    }
+]
+
+const index = 2;
+
+const final = [
+    ...users.slice(0, index),
+    {...users[2],gender:"female"},
+    ...users.slice(index + 1),
+];
+
+console.log(final); //[
+  { name: 'prince', age: 22, gender: 'male' },
+  { name: 'sam', age: 24, gender: 'female' },
+  { name: 'pritvi', age: 20, gender: 'female' }
+]
+
+console.log(users);     //[ { name: 'prince', age: 22, gender: 'male' }, { name: 'sam', age: 24, gender: 'female' },{name: 'pritvi', age: 20, gender: 'unkonwn' } ]
 
 
 
--- javascript/objects.js
+++ javascript/objects.js
@@ -27,7 +27,99 @@ console.log(newObj);
 console.log(obj);
 
 
-=> 
+=> immutably change the data
+
+const users = [
+    {
+        name : 'prince',
+        age: 22,
+        gender:'male'
+    },
+    {
+        name : 'sam',
+        age : 24,
+        gender : 'female'
+    },
+    {
+        name : "pritvi",
+        age : 20,
+        gender:'unkonwn'
+    }
+]
+
+const index = 2;
+
+const final = [
+    ...users.slice(0, index),
+    {...users[2],gender:"female"},
+    ...users.slice(index + 1),
+];
+
+console.log(final); //[
+  { name: 'prince', age: 22, gender: 'male' },
+  { name: 'sam', age: 24, gender: 'female' },
+  { name: 'pritvi', age: 20, gender: 'female' }
+]
+
+console.log(users);     //[ { name: 'prince', age: 22, gender: 'male' }, { name: 'sam', age: 24, gender: 'female' },{name: 'pritvi', age: 20, gender: 'unkonwn' } ]
+
+
+
+const users = [
+    {
+        name : 'prince',
+        age: 22,
+        gender:'male'
+    },
+    {
+        name : 'sam',
+        age : 24,
+        gender : 'female'
+    },
+    {
+        name : "pritvi",
+        age : 20,
+        gender:'unkonwn'
+    }
+]
+
+const newUser = [
+    {
+        name : 'alia',
+        age : 28,
+        gender : 'female'
+    },
+    {
+        name : 'rohan',
+        age : 29,
+        gender : "male"
+    }
+]
+
+const allUser = users.concat(newUser)
+
+const fullarr = [...users, ...newUser]
+
+console.log(fullarr);
+
+console.log(allUser);
+
+
+const index = 2;
+
+const final = [
+    ...users.slice(0, index),
+    {...users[2],gender:"female"},
+    ...users.slice(index + 1),
+];
+
+console.log(final);
+
+console.log(users);
+
+const newarr = users.flat();
+
+console.log(newarr);
 
 
 
